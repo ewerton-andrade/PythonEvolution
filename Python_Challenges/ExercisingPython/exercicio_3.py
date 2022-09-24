d6b9e9554add9fcbff53ca0b7f5a5d91b32b4435
ewerton.andrade2 <ewerton.andrade@beegol.com>
2022-09-24 12:43:20 -0400

    Update exercicio_3.py

--- Python_Challenges/ExercisingPython/exercicio_3.py
+++ Python_Challenges/ExercisingPython/exercicio_3.py
@@ -52,6 +52,7 @@ HK
 """
 
 from itertools import combinations
+from re import L
 
 alfabet = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9, "J":10, "K":11, 
 "L":12, "M":13, "N":14, "O": 15, "P":16, "Q":17, "R":18, "S":19, "T":20, "U":21, "V":22, 
@@ -60,7 +61,7 @@ alfabet = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, "H":8, "I":9, "J":10
 S = "HACK"
 k = 2
 lista = list(combinations(S, k))
-#print(lista)
+print(lista)
 
 # iterar cada letra no alfabeto e retornar o número correspondente a sua posicao no alfabeto
 S_order = []
@@ -75,3 +76,38 @@ print(S_order)
 #now sorted (S_order): [1,3,8,11]
 S_order.sort()
 print(S_order)
+#agora substituir os indices da lista S_order (apos sorted em ordem crescente) pelas respectivas letras do dicionario
+# l = list(map(lambda x: x.replace(alfabet.keys, alfabet.values), S_order))
+# print(l)
+
+for i in S_order:
+    for k,v in alfabet.items():
+        if i == v:
+            #acho o index em que o elemento está para que eu o substitua pelo index logo em seguida
+            # print(f"O element {i} está na posiçao {S_order.index(i)} da list {S_order}")
+            S_order[S_order.index(i)] = k
+
+print(f"A lista final está assim: {S_order}")
+# for i in S_order:
+#     print(i)
+# l = lista = list(combinations(S, k))
+
+#agora eu preciso aglutinar todos os elementos da lista em uma unica string
+S= "".join(S_order) # Ok
+print(f"S: {S}")
+
+#agora eu preciso printar uma combinacao 1 a 1
+k=1
+l = list(combinations(S,k))
+for i in l:
+    print(i) 
+# agora eu preciso saber como printar somente o valor da String sem as aspas, virgula e parentesis
+#como é uma tupla eu posso utilizar os metodos para imprimir somente o primeiro valor
+for i in l:
+    print(i[0])
+
+# agora eu preciso fazer uma combinaçäo dois a dois nos elementos da string e imprimir somente o primeiro elemento
+k=2
+l2 = list(combinations(S,k))
+for i in l2:
+    print(f"{i[0]}{i[1]}")
